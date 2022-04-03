15b9b07a8011b240726e091b47b3805b1e2594ed
Mark Lawlor <mwlawlor@gmail.com>
2022-04-03 20:01:36 +1000

    fix: add seperate web provider

---
+++ src/provider.web.tsx
@@ -0,0 +1,42 @@
+import { PropsWithChildren, useState } from "react";
+import { ColorSchemeName } from "react-native";
+import { Appearance } from "react-native-web";
+
+import {
+  TailwindStyleContext,
+  TailwindColorSchemeContext,
+  TailwindSetColorSchemeContext,
+  StyleRecord,
+  MediaRules,
+} from "./context";
+
+export interface TailwindProviderProps {
+  styles?: StyleRecord;
+  media?: MediaRules;
+  colorScheme?: ColorSchemeName;
+}
+
+export function TailwindProvider({
+  styles = {},
+  media = {},
+  colorScheme: overrideColorScheme,
+  children,
+}: PropsWithChildren<TailwindProviderProps>) {
+  // Save into state, as this will never change
+  const [styleValue] = useState<TailwindStyleContext>({ media, styles });
+  const [colorScheme, setColorScheme] = useState<ColorSchemeName>(
+    Appearance.getColorScheme() ?? "light"
+  );
+
+  return (
+    <TailwindStyleContext.Provider value={styleValue}>
+      <TailwindColorSchemeContext.Provider
+        value={overrideColorScheme || colorScheme}
+      >
+        <TailwindSetColorSchemeContext.Provider value={setColorScheme}>
+          {children}
+        </TailwindSetColorSchemeContext.Provider>
+      </TailwindColorSchemeContext.Provider>
+    </TailwindStyleContext.Provider>
+  );
+}
