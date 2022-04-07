49c8e0105ee3aa4ecd782038301b3aba25ba78e8
Mark Lawlor <mwlawlor@gmail.com>
2022-04-07 11:48:05 +1000

    make __useParseTailwind params options

--- src/use-parse-tailwind.ts
+++ src/use-parse-tailwind.ts
@@ -23,7 +23,7 @@ export function __useParseTailwind(
   {
     styles: additionalStyles = {},
     media: additionalMediaRules = {},
-  }: UseParseTailwindOptions
+  }: UseParseTailwindOptions = {}
 ) {
   const { styles, media: mediaRules } = useContext(TailwindStyleContext);
   const colorScheme = useContext(TailwindColorSchemeContext);
