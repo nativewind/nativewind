d2485c0ddab133cd5f8ca7ac74a56054216dd606
Mark Lawlor <mwlawlor@gmail.com>
2022-04-26 15:12:54 +1000

    chore: cleanup deps

--- src/babel/transforms/append-variables.ts
+++ src/babel/transforms/append-variables.ts
@@ -1,11 +1,18 @@
 import {
+  arrayExpression,
+  booleanLiteral,
   callExpression,
   expressionStatement,
   identifier,
   memberExpression,
+  nullLiteral,
+  numericLiteral,
+  objectExpression,
+  objectProperty,
   Statement,
+  stringLiteral,
+  unaryExpression,
 } from "@babel/types";
-import serialize from "babel-literal-to-ast";
 import { MediaRecord, StyleRecord } from "../types";
 import { globalMedia, globalStyle } from "./constants";
 
@@ -37,4 +44,34 @@ export function appendVariables(
       )
     )
   );
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function serialize(literal: any): any {
+  if (literal === null) {
+    return nullLiteral();
+  }
+  switch (typeof literal) {
+    case "number":
+      return numericLiteral(literal);
+    case "string":
+      return stringLiteral(literal);
+    case "boolean":
+      return booleanLiteral(literal);
+    case "undefined":
+      return unaryExpression("void", numericLiteral(0), true);
+    default:
+      if (Array.isArray(literal)) {
+        return arrayExpression(literal.map((n) => serialize(n)));
+      }
+      return objectExpression(
+        Object.keys(literal)
+          .filter((k) => {
+            return typeof literal[k] !== "undefined";
+          })
+          .map((k) => {
+            return objectProperty(stringLiteral(k), serialize(literal[k]));
+          })
+      );
+  }
 }
--- src/types/node-modules.d.ts
+++ src/types/node-modules.d.ts
@@ -5,11 +5,3 @@ declare module "postcss-css-variables" {
 declare module "postcss-color-rgb" {
   export default unknown;
 }
-
-declare module "postcss-rem-to-pixel" {
-  export default unknown;
-}
-
-declare module "babel-literal-to-ast" {
-  export default unknown;
-}
