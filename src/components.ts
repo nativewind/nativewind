02527fc870844529c507972382e9a5cc7f3a2c61
Mark Lawlor <mwlawlor@gmail.com>
2022-05-12 11:06:52 +1000

    feat: export pre-styled React Native components

---
+++ src/components.ts
@@ -0,0 +1,52 @@
+import { styled } from "./styled";
+import {
+  ActivityIndicator as RNActivityIndicator,
+  Button as RNButton,
+  FlatList as RNFlatList,
+  Image as RNImage,
+  ImageBackground as RNImageBackground,
+  KeyboardAvoidingView as RNKeyboardAvoidingView,
+  Modal as RNModal,
+  Pressable as RNPressable,
+  RefreshControl as RNRefreshControl,
+  ScrollView as RNScrollView,
+  SectionList as RNSectionList,
+  StatusBar as RNStatusBar,
+  Switch as RNSwitch,
+  Text as RNText,
+  TextInput as RNTextInput,
+  TouchableHighlight as RNTouchableHighlight,
+  TouchableOpacity as RNTouchableOpacity,
+  TouchableWithoutFeedback as RNTouchableWithoutFeedback,
+  View as RNView,
+  VirtualizedList as RNVirtualizedList,
+  DrawerLayoutAndroid as RNDrawerLayoutAndroid,
+  TouchableNativeFeedback as RNTouchableNativeFeedback,
+  InputAccessoryView as RNInputAccessoryView,
+  SafeAreaView as RNSafeAreaView,
+} from "react-native";
+
+export const ActivityIndicator = styled(RNActivityIndicator);
+export const Button = styled(RNButton);
+export const FlatList = styled(RNFlatList);
+export const Image = styled(RNImage);
+export const ImageBackground = styled(RNImageBackground);
+export const KeyboardAvoidingView = styled(RNKeyboardAvoidingView);
+export const Modal = styled(RNModal);
+export const Pressable = styled(RNPressable);
+export const RefreshControl = styled(RNRefreshControl);
+export const ScrollView = styled(RNScrollView);
+export const SectionList = styled(RNSectionList);
+export const StatusBar = styled(RNStatusBar);
+export const Switch = styled(RNSwitch);
+export const Text = styled(RNText);
+export const TextInput = styled(RNTextInput);
+export const TouchableHighlight = styled(RNTouchableHighlight);
+export const TouchableOpacity = styled(RNTouchableOpacity);
+export const TouchableWithoutFeedback = styled(RNTouchableWithoutFeedback);
+export const View = styled(RNView);
+export const VirtualizedList = styled(RNVirtualizedList);
+export const DrawerLayoutAndroid = styled(RNDrawerLayoutAndroid);
+export const TouchableNativeFeedback = styled(RNTouchableNativeFeedback);
+export const InputAccessoryView = styled(RNInputAccessoryView);
+export const SafeAreaView = styled(RNSafeAreaView);
