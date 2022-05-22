1571c60dac1e92ef1131ef3885bda219f61c71a9
Mark Lawlor <mwlawlor@gmail.com>
2022-05-22 11:38:05 +1000

    refactor: remove initial hooks from public api

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,4 @@ export * from "./styled";
 export * from "./styled-component";
 export { useColorScheme } from "./context/color-scheme";
 export { useComponent, ComponentContext } from "./context/component";
-export { useDeviceMedia } from "./context/device-media";
 export { usePlatform } from "./context/platform";
-export { useStyleSheet } from "./context/style-sheet";
