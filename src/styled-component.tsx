ab9496a931e5176afec7a951a9917df534e0bbf3
Mark Lawlor <mwlawlor@gmail.com>
2022-05-23 18:46:56 +1000

    refactor: remove styledOptions from StyledComponent

--- src/styled-component.tsx
+++ src/styled-component.tsx
@@ -1,17 +1,14 @@
-import { styled, StyledOptions } from "./styled";
+import { styled } from "./styled";
 import { Component, StyledProps } from "./utils/styled";
 
 export type StyledComponentProps<P> = StyledProps<P> & {
   component: Component<P>;
-  styledOptions?: StyledOptions<P>;
 };
 
 export function StyledComponent<P>({
   component,
-  styledOptions,
   ...options
 }: StyledComponentProps<P>) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- I'm too tired to type this
-  const Component = styled<P>(component, styledOptions as any);
+  const Component = styled<P>(component);
   return <Component {...(options as P)} />;
 }
