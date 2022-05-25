467f7d2883d614e195a17231c565d75caa190cc5
Mark Lawlor <mwlawlor@gmail.com>
2022-05-26 09:06:53 +1000

    fix: add import React to component files

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,4 +1,5 @@
-import React, { PropsWithChildren, useState } from "react";
+import * as React from "react";
+import { PropsWithChildren, useState } from "react";
 import {
   useWindowDimensions,
   ColorSchemeName,
