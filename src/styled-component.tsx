2745669296356cac9a1354703a163a793c319ea0
Mark Lawlor <mwlawlor@gmail.com>
2022-05-31 11:29:53 +1000

    fix: typing of forwardRef override

--- src/styled-component.tsx
+++ src/styled-component.tsx
@@ -6,11 +6,17 @@ export type StyledComponentProps<P> = StyledProps<P> & {
   component: React.ComponentType<P>;
 };
 
+/**
+ * This might cause issues in the future, but we provide an override of forwardRef
+ * to provide better typing of our components
+ */
 declare module "react" {
   // eslint-disable-next-line @typescript-eslint/ban-types
   function forwardRef<T, P = {}>(
     render: (props: P, ref: React.Ref<T>) => React.ReactElement | null
-  ): (props: P & React.RefAttributes<T>) => React.ReactElement | null;
+  ): React.ForwardRefExoticComponent<
+    React.PropsWithoutRef<P> & React.RefAttributes<T>
+  >;
 }
 
 function StyledComponentFunction<P>(
