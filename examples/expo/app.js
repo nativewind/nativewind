918b7569f175a99fd40fba33f56b4d95849a5a07
Mark Lawlor <mwlawlor@gmail.com>
2022-08-10 18:09:06 -0300

    refactor: move example into examples folder

---
+++ examples/expo/app.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
