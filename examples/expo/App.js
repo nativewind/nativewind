7e037b33736c1cb59b91bdf6f9e701f74ed1542a
Mark Lawlor <mwlawlor@gmail.com>
2022-08-22 06:18:19 +1000

    docs: update custom components

--- examples/expo/App.js
+++ examples/expo/App.js
@@ -1,8 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
-import "./main.css";
-
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
