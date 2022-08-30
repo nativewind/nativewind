7efa865fd700735fea752ae82304c55af8a4f37b
FerasAloudah <fireslay@gmail.com>
2022-08-30 23:06:35 +0300

    fix: handle ts errors

--- packages/nativewind/src/styled/with-styled-children.ts
+++ packages/nativewind/src/styled/with-styled-children.ts
@@ -1,4 +1,5 @@
-import { ReactNode, Children, cloneElement } from "react";
+import { ReactNode, Children, cloneElement, isValidElement } from "react";
+import { StyleProp } from "react-native";
 import { isFragment } from "react-is";
 import { StylesArray, StyleSheetRuntime } from "../style-sheet";
 import { matchesMask, PARENT } from "../utils/selector";
@@ -31,7 +32,7 @@ export function withStyledChildren({
     : componentChildren;
 
   return Children.toArray(children).filter((child) => Boolean(child)).map((child, index) => {
-    if (!child || !child.props) {
+    if (!child || !isValidElement<{ style?: StyleProp<any> }>(child)) {
       return child;
     }
 
