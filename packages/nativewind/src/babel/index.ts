f1eb4200ffefaeb7f0363516d7b1e81873ed9b93
Osama Qarem <osamaqarem@gmail.com>
2022-09-10 20:30:56 +0800

    Check for empty `safelist` array instead of undefined

--- packages/nativewind/src/babel/index.ts
+++ packages/nativewind/src/babel/index.ts
@@ -130,12 +130,14 @@ export default function (
 
         if (!canCompile) return;
 
+        const safelist = tailwindConfig.safelist.length > 0 ? tailwindConfig.safelist : ["babel-empty"]
+        
         const output = extractStyles({
           ...tailwindConfig,
           content: [filename],
           // If the file doesn't have any Tailwind styles, it will print a warning
           // We force an empty style to prevent this
-          safelist: tailwindConfig.safelist ?? ["babel-empty"],
+          safelist,
         });
 
         if (!output.hasStyles) return;
