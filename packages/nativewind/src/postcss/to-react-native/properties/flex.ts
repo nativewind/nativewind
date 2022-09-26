ca00130b48c19f83bbcf31713dd1ab26536f45ca
mateusz-ramski <65708055+mateusz-ramski@users.noreply.github.com>
2022-09-26 12:15:13 +0200

    fix: added missing flexBasis to postcss flex property (#255)
    * fix: added missing flexBasis to postcss flex property
    
    * add changeset
    
    Co-authored-by: Mark Lawlor <mwlawlor@gmail.com>

--- packages/nativewind/src/postcss/to-react-native/properties/flex.ts
+++ packages/nativewind/src/postcss/to-react-native/properties/flex.ts
@@ -2,7 +2,7 @@ import { getStylesForProperty } from "css-to-react-native";
 import { PropertyFunction } from "./only";
 
 export const flex: PropertyFunction<"flex"> = (value, name) => {
-  const { flexGrow, flexShrink } = getStylesForProperty(name, value);
-  return { flexGrow, flexShrink };
+  const { flexGrow, flexShrink, flexBasis } = getStylesForProperty(name, value);
+  return { flexGrow, flexShrink, flexBasis };
 };
 flex.prop = "flex";
