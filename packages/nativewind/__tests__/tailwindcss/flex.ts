37e1ca09925d16e05bc110a1ec9b836a911c0d14
Mark Lawlor <mwlawlor@gmail.com>
2022-09-27 21:36:40 +1000

    chore: update snapshots

--- packages/nativewind/__tests__/tailwindcss/flex.ts
+++ packages/nativewind/__tests__/tailwindcss/flex.ts
@@ -2,10 +2,28 @@ import { tailwindRunner } from "./runner";
 
 tailwindRunner("Layout - Flex", [
   ["flex", { styles: { flex: { display: "flex" } } }],
-  ["flex-auto", { styles: { "flex-auto": { flexGrow: 1, flexShrink: 1 } } }],
+  [
+    "flex-auto",
+    {
+      styles: {
+        "flex-auto": { flexBasis: "auto", flexGrow: 1, flexShrink: 1 },
+      },
+    },
+  ],
   [
     "flex-initial",
-    { styles: { "flex-initial": { flexGrow: 0, flexShrink: 1 } } },
+    {
+      styles: {
+        "flex-initial": { flexBasis: "auto", flexGrow: 0, flexShrink: 1 },
+      },
+    },
   ],
-  ["flex-none", { styles: { "flex-none": { flexGrow: 0, flexShrink: 0 } } }],
+  [
+    "flex-none",
+    {
+      styles: {
+        "flex-none": { flexBasis: "auto", flexGrow: 0, flexShrink: 0 },
+      },
+    },
+  ],
 ]);
