7b485ec6962e72018f4c762566f42107b29d3a5f
Mark Lawlor <mwlawlor@gmail.com>
2022-10-21 10:13:40 +1000

    fix: expo snack id selectors

--- packages/nativewind/src/expo-snack.tsx
+++ packages/nativewind/src/expo-snack.tsx
@@ -11,40 +11,6 @@ export function withExpoSnack(
     const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
-      if (Platform.OS === "web" && config) {
-        const head = document.querySelectorAll("head")[0];
-
-        let configScript = document.querySelectorAll(
-          "#nativewind_config"
-        )[0] as HTMLScriptElement;
-        if (!configScript) {
-          configScript = document.createElement("script");
-          configScript.id = "nativewind_config";
-          configScript.type = "text/javascript";
-          head.append(configScript);
-        }
-        configScript.text = `tailwind.config = ${JSON.stringify(config)}`;
-      }
-    }, [config]);
-
-    useEffect(() => {
-      if (Platform.OS === "web" && css) {
-        const head = document.querySelectorAll("head")[0];
-
-        let cssStyle = document.querySelectorAll(
-          "#nativewind_css"
-        )[0] as HTMLStyleElement;
-        if (!cssStyle) {
-          cssStyle = document.createElement("style");
-          cssStyle.id = "nativewind_css";
-          cssStyle.type = "text/tailwindcss";
-          head.append(cssStyle);
-        }
-        cssStyle.replaceChildren(document.createTextNode(css));
-      }
-    }, [css]);
-
-    useEffect(() => {
       if (Platform.OS === "web") {
         const head = document.querySelectorAll("head")[0];
         const playCDN = document.createElement("script");
@@ -52,8 +18,37 @@ export function withExpoSnack(
         playCDN.src = "https://cdn.tailwindcss.com";
         playCDN.addEventListener("load", () => setLoaded(true));
         head.append(playCDN);
+
+        const configScript = document.createElement("script");
+        configScript.id = "nativewind_config";
+        configScript.type = "text/javascript";
+        head.append(configScript);
+
+        const cssStyle = document.createElement("style");
+        cssStyle.id = "nativewind_css";
+        cssStyle.type = "text/tailwindcss";
+        head.append(cssStyle);
       }
     }, []);
+
+    useEffect(() => {
+      if (Platform.OS === "web" && config) {
+        const configScript = document.querySelectorAll(
+          "#nativewind_config"
+        )[0] as HTMLScriptElement;
+        configScript.text = `tailwind.config = ${JSON.stringify(config)}`;
+      }
+    }, [config]);
+
+    useEffect(() => {
+      if (Platform.OS === "web" && css) {
+        const cssStyle = document.querySelectorAll(
+          "#nativewind_css"
+        )[0] as HTMLStyleElement;
+        cssStyle.replaceChildren(document.createTextNode(css));
+      }
+    }, [css]);
+
     return loaded ? <Component /> : undefined;
   };
 }
