aa6ab31e1cb4c27fe7b7bbcbbb922c338233cedf
Mark Lawlor <mwlawlor@gmail.com>
2022-10-21 10:08:14 +1000

    fix: expo snack id selectors

--- packages/nativewind/experiment.sh
+++ packages/nativewind/experiment.sh
@@ -2,5 +2,5 @@
 
 npm run build && \
 npm version $(git describe --tags) --no-git-tag-version;
-npm publish --tag experiment && sleep 30 && \
+npm publish --tag experiment && sleep 45 && \
 curl "https://snackager.expo.io/bundle/nativewind@$(git describe --tags)?platforms=ios,android,web&version_snackager=true&bypassCache=true" --retry 5
--- packages/nativewind/src/expo-snack.tsx
+++ packages/nativewind/src/expo-snack.tsx
@@ -1,18 +1,57 @@
 import { ComponentType, useEffect, useState } from "react";
 import { Platform } from "react-native";
+import { Config } from "tailwindcss";
 
-export function withExpoSnack(Component: ComponentType) {
+export function withExpoSnack(
+  Component: ComponentType,
+  config?: Config,
+  css?: string
+) {
   return () => {
     const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
+      if (Platform.OS === "web" && config) {
+        const head = document.querySelectorAll("head")[0];
+
+        let configScript = document.querySelectorAll(
+          "#nativewind_config"
+        )[0] as HTMLScriptElement;
+        if (!configScript) {
+          configScript = document.createElement("script");
+          configScript.id = "nativewind_config";
+          configScript.type = "text/javascript";
+          head.append(configScript);
+        }
+        configScript.text = `tailwind.config = ${JSON.stringify(config)}`;
+      }
+    }, [config]);
+
+    useEffect(() => {
+      if (Platform.OS === "web" && css) {
+        const head = document.querySelectorAll("head")[0];
+
+        let cssStyle = document.querySelectorAll(
+          "#nativewind_css"
+        )[0] as HTMLStyleElement;
+        if (!cssStyle) {
+          cssStyle = document.createElement("style");
+          cssStyle.id = "nativewind_css";
+          cssStyle.type = "text/tailwindcss";
+          head.append(cssStyle);
+        }
+        cssStyle.replaceChildren(document.createTextNode(css));
+      }
+    }, [css]);
+
+    useEffect(() => {
       if (Platform.OS === "web") {
         const head = document.querySelectorAll("head")[0];
-        const script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "https://cdn.tailwindcss.com";
-        script.addEventListener("load", () => setLoaded(true));
-        head.append(script);
+        const playCDN = document.createElement("script");
+        playCDN.type = "text/javascript";
+        playCDN.src = "https://cdn.tailwindcss.com";
+        playCDN.addEventListener("load", () => setLoaded(true));
+        head.append(playCDN);
       }
     }, []);
     return loaded ? <Component /> : undefined;
