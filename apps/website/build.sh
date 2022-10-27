2d65951693b307ea4bd3a64319fdba25b6cbd53a
Mark Lawlor <mwlawlor@gmail.com>
2022-10-27 22:19:15 +1000

    fix: preview compile

--- apps/website/build.sh
+++ apps/website/build.sh
@@ -1,4 +1,5 @@
 if [ "$VERCEL_ENV" == "preview" ]
+then
   npm version prerelease -w nativewind --preid $(git describe --tags --always) --no-git-tag-version
   npm pkg get -w nativewind > ../node_modules/nativewind/package.json
 fi
