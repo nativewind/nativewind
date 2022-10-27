a5ab052229f69e517d048217ea7d6f7327a0b091
Mark Lawlor <mwlawlor@gmail.com>
2022-10-27 18:43:27 +1000

    ci: build on vercel

--- apps/website/build.sh
+++ apps/website/build.sh
@@ -1,3 +1,4 @@
 npm version prerelease -w nativewind --preid $(git describe --tags --always) --no-git-tag-version
+npm pkg get -w nativewind > ../node_modules/nativewind/package.json
 docusaurus build
 
