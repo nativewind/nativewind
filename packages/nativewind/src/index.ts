e5dac75b2c8a7fe9c9e6b1215c77f893e15bc84a
Mark Lawlor <mwlawlor@gmail.com>
2022-11-03 08:50:04 +1000

    fix: variants export

--- packages/nativewind/src/index.ts
+++ packages/nativewind/src/index.ts
@@ -3,3 +3,4 @@ export * from "./theme-functions";
 export { NativeWindStyleSheet, useUnsafeVariable } from "./style-sheet";
 export { useColorScheme } from "./use-color-scheme";
 export { withExpoSnack } from "./expo-snack";
+export { cva as variants } from "class-variance-authority";
