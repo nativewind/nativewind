c9f60d47518a871cd83fd406b8ff4d17450a4f77
Mark Lawlor <mwlawlor@gmail.com>
2022-11-23 07:55:43 +1000

    fix: platform variant intellisense

--- packages/nativewind/src/tailwind/web/index.ts
+++ packages/nativewind/src/tailwind/web/index.ts
@@ -1,11 +1,22 @@
 import { Config } from "tailwindcss";
 import plugin from "tailwindcss/plugin";
 
+const unsupportedVariants = ["ios", "android", "windows", "macos", "native"];
+
 const preset: Config = {
   content: [],
   plugins: [
     plugin(function ({ addVariant }) {
       addVariant("web", "&");
+
+      // Your editors intellisense loads the web plugin
+      // so we need to include the native only variants
+      // unfortunately this means they are preset in the production CSS
+      // but putting them inside an invalid media-query means they won't
+      // be used
+      for (const variant of unsupportedVariants) {
+        addVariant(variant, "@media unsupported");
+      }
     }),
   ],
 };
