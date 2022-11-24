f648d382c5b7888bc81423f619040f4e196175d2
Mark Lawlor <mwlawlor@gmail.com>
2022-11-24 16:39:03 +1000

    fix: nativewind preset function fixes

--- packages/nativewind/src/tailwind/native/index.ts
+++ packages/nativewind/src/tailwind/native/index.ts
@@ -15,88 +15,99 @@ import { space } from "./plugins/space";
 import { translate } from "./plugins/translate";
 import { variables } from "./plugins/variables";
 
-export default function nativewindPreset({ gapPolyfill = true }) {
-  const plugins = [
-    boxShadow,
-    darkMode,
-    divide,
-    elevation,
-    fontSize,
-    parent,
-    platforms,
-    rotate,
-    scale,
-    skew,
-    space,
-    translate,
-    variables,
-  ];
+const plugins = [
+  boxShadow,
+  darkMode,
+  divide,
+  elevation,
+  fontSize,
+  parent,
+  platforms,
+  rotate,
+  scale,
+  skew,
+  space,
+  translate,
+  variables,
+];
 
-  const corePlugins: Config["corePlugins"] = {
-    backgroundOpacity: false,
-    borderOpacity: false,
-    boxShadow: false,
-    boxShadowColor: false,
-    divideColor: false,
-    divideOpacity: false,
-    divideStyle: false,
-    divideWidth: false,
-    fontSize: false,
-    placeholderOpacity: false,
-    ringOpacity: false,
-    rotate: false,
-    scale: false,
-    skew: false,
-    space: false,
-    textOpacity: false,
-    translate: false,
+const corePlugins: Config["corePlugins"] & Record<string, unknown> = {
+  backgroundOpacity: false,
+  borderOpacity: false,
+  boxShadow: false,
+  boxShadowColor: false,
+  divideColor: false,
+  divideOpacity: false,
+  divideStyle: false,
+  divideWidth: false,
+  fontSize: false,
+  placeholderOpacity: false,
+  ringOpacity: false,
+  rotate: false,
+  scale: false,
+  skew: false,
+  space: false,
+  textOpacity: false,
+  translate: false,
+};
+
+const preset: Config = {
+  content: [],
+  theme: {
+    extend: {
+      aspectRatio: {
+        auto: "0",
+        square: "1",
+        video: "1.777777778",
+      },
+      letterSpacing: {
+        tighter: "-0.5px",
+        tight: "-0.25px",
+        normal: "0px",
+        wide: "0.25px",
+        wider: "0.5px",
+        widest: "1px",
+      },
+      elevation: {
+        sm: "1.5",
+        DEFAULT: "3",
+        md: "6",
+        lg: "7.5",
+        xl: "12.5",
+        "2xl": "25",
+        none: "0",
+      },
+      boxShadow: {
+        sm: "0px 1px 2px rgba(0, 0, 0, 0.1)",
+        DEFAULT: "0px 2px 6px rgba(0, 0, 0, 0.1)",
+        md: "0px 6px 10px rgba(0, 0, 0, 0.1)",
+        lg: "0px 10px 15px rgba(0, 0, 0, 0.1)",
+        xl: "0px 20px 25px rgba(0, 0, 0, 0.1)",
+        "2xl": "0px 25px 50px rgba(0, 0, 0, 0.1)",
+        none: "0px 0px 0px rgba(0, 0, 0, 0)",
+      },
+    },
+  },
+  plugins,
+  corePlugins,
+};
+
+function nativewindPreset({ gapPolyfill = true } = {}) {
+  // Duplicating the preset allows tests to run with different config
+  const duplicatePreset = {
+    ...preset,
+    plugins: [...plugins],
+    corePlugins: { ...corePlugins },
   };
 
   if (gapPolyfill) {
-    plugins.push(gap);
-    corePlugins.gap = false;
+    duplicatePreset.plugins.push(gap);
+    duplicatePreset.corePlugins.gap = false;
   }
 
-  const preset: Config = {
-    content: [],
-    theme: {
-      extend: {
-        aspectRatio: {
-          auto: "0",
-          square: "1",
-          video: "1.777777778",
-        },
-        letterSpacing: {
-          tighter: "-0.5px",
-          tight: "-0.25px",
-          normal: "0px",
-          wide: "0.25px",
-          wider: "0.5px",
-          widest: "1px",
-        },
-        elevation: {
-          sm: "1.5",
-          DEFAULT: "3",
-          md: "6",
-          lg: "7.5",
-          xl: "12.5",
-          "2xl": "25",
-          none: "0",
-        },
-        boxShadow: {
-          sm: "0px 1px 2px rgba(0, 0, 0, 0.1)",
-          DEFAULT: "0px 2px 6px rgba(0, 0, 0, 0.1)",
-          md: "0px 6px 10px rgba(0, 0, 0, 0.1)",
-          lg: "0px 10px 15px rgba(0, 0, 0, 0.1)",
-          xl: "0px 20px 25px rgba(0, 0, 0, 0.1)",
-          "2xl": "0px 25px 50px rgba(0, 0, 0, 0.1)",
-          none: "0px 0px 0px rgba(0, 0, 0, 0)",
-        },
-      },
-    },
-    plugins,
-    corePlugins,
-  };
-
-  return preset;
+  return duplicatePreset;
 }
+
+// All the preset so be used as an object or a function
+// export default Object.assign(nativewindPreset, preset);
+export default Object.assign(nativewindPreset, preset);
