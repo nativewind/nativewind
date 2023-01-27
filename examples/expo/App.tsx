d8a9962edf7bb23f083b7d2f8bb4f6e1cdd720d7
Eric Kreutzer <eric@lugg.com>
2023-01-27 10:14:26 -0700

    update example app to use nativewind@next

--- examples/expo/App.tsx
+++ examples/expo/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, LogBox } from "react-native";
 
 import "./styles.css";
 
+LogBox.ignoreLogs(["Require cycles are allowed"]);
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center bg-red-100">
       <Text>Open up App.js to start working on your app!!</Text>
     </View>
   );
--- examples/expo/tailwind.config.js
+++ examples/expo/tailwind.config.js
@@ -1,6 +1,9 @@
+const nativewind = require("nativewind/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./App.tsx"],
+  presets: [nativewind],
   theme: {
     extend: {},
   },
