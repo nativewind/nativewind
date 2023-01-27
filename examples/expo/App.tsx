ec6e4ab9615551b35d64498bd2eea58a1c1ccd66
Eric Kreutzer <eric@lugg.com>
2023-01-27 11:00:26 -0700

    update example to demonstrate features

--- examples/expo/App.tsx
+++ examples/expo/App.tsx
@@ -1,16 +1,42 @@
+import { useColorScheme } from "nativewind";
 import React from "react";
-import { LogBox, Text, View } from "react-native";
+import {
+  LogBox,
+  Text,
+  View,
+  Switch,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
 
 import "./styles.css";
 
 LogBox.ignoreLogs(["Require cycles are allowed"]);
 
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+
   return (
-    <View className="flex-1 items-center justify-center bg-red-100">
-      <Text className="font-bold">
-        Open up App.js to start working on your app!!
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-background">
+      <StatusBar
+        barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
+      />
+      <View className="p-4 flex-row items-center justify-center gap-4">
+        <Text className="font-semibold text-foreground">Light</Text>
+        <Switch
+          value={colorScheme === "dark"}
+          onChange={() => toggleColorScheme()}
+        />
+        <Text className="font-semibold text-foreground">Dark</Text>
+      </View>
+      <View className="flex-1 justify-center px-4 space-y-4">
+        <Text className="font-bold text-foreground text-center">
+          Welcome to NativeWind!
+        </Text>
+        <Text className="border-hairline border-foreground rounded-xl p-3 font-mono text-foreground">
+          {JSON.stringify({ colorScheme }, undefined, 2)}
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
--- examples/expo/tailwind.config.js
+++ examples/expo/tailwind.config.js
@@ -1,11 +1,38 @@
 const nativewind = require("nativewind/tailwind");
+const {
+  platformSelect,
+  hairlineWidth,
+} = require("nativewind/dist/runtime/native/theme-functions");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./App.tsx"],
   presets: [nativewind],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        hairline: hairlineWidth(),
+      },
+      colors: {
+        background: "var(--background-color)",
+        foreground: "var(--foreground-color)",
+      },
+    },
+    fontFamily: {
+      mono: platformSelect({
+        ios: "Menlo-Regular",
+        android: "monospace",
+      }),
+    },
+    variables: {
+      "--background-color": "white",
+      "--foreground-color": "black",
+    },
+    darkVariables: {
+      "--background-color": "black",
+      "--foreground-color": "white",
+    },
   },
   plugins: [],
 };
