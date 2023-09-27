9e62377613c19c4d47637c3111835824647e7e81
Mark Lawlor <mwlawlor@gmail.com>
2023-09-27 15:02:05 +1000

    website: debug deploy

--- apps/website/api/compile.ts
+++ apps/website/api/compile.ts
@@ -41,13 +41,13 @@ const handler: VercelApiHandler = async (request, response) => {
 
   let { css: output } = await postcss([
     tailwind({
-      theme: {,
+      theme: {},
       presets: [preset as any],
       plugins: [tailwindcssContainerQueries],
       content: [{ raw: request.body.content, extension: "html" }],
       corePlugins: {
-        preflight: false
-      }
+        preflight: false,
+      },
     }),
   ]).process("@tailwind components;@tailwind utilities;", {
     from: undefined,
