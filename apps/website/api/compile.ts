4d0b734cde72adf13af96fc611dff5eecf2459d4
Mark Lawlor <mwlawlor@gmail.com>
2023-09-27 13:20:53 +1000

    website: attempt fix /api/compile

--- apps/website/api/compile.ts
+++ apps/website/api/compile.ts
@@ -2,7 +2,7 @@ import postcss from "postcss";
 import tailwind from "tailwindcss";
 import tailwindcssContainerQueries from "@tailwindcss/container-queries";
 import { preset } from "nativewind/preset";
-import { cssToReactNativeRuntime } from "react-native-css-interop/dist/css-to-rn";
+import { cssToReactNativeRuntime } from "react-native-css-interop/css-to-rn";
 
 import "lightningcss/node/lightningcss.linux-x64-gnu.node";
 
