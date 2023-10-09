52f25cfe5d4a11e810803eef5598c4725bb1983f
Mark Lawlor <mwlawlor@gmail.com>
2023-10-09 11:57:36 +1000

    fix dependencies

--- examples/expo-router/babel.config.js
+++ examples/expo-router/babel.config.js
@@ -1,18 +1,19 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "nativewind/babel"],
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          jsxImportSource: "nativewind",
+        },
+      ],
+      "nativewind/babel",
+    ],
     plugins: [
       // Required for expo-router
       "expo-router/babel",
       "react-native-reanimated/plugin",
-      [
-        "@babel/plugin-transform-react-jsx",
-        {
-          runtime: "automatic",
-          importSource: "nativewind",
-        },
-      ],
     ],
   };
 };
