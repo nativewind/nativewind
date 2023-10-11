7324b4c3a802c35385a71841a7409ec86bab7391
Mark Lawlor <mwlawlor@gmail.com>
2023-10-12 09:14:12 +1000

    css-interop: fix exports

--- packages/react-native-css-interop/src/exports.ts
+++ packages/react-native-css-interop/src/exports.ts
@@ -1,6 +1,3 @@
-/**
- * @deprecated This is an alias for React.createElement. Just use it directly
- */
-
 export { createElement } from "react";
+export { createElementAndCheckCssInterop } from "./runtime/render";
 export { cssInterop, remapProps, unstable_styled } from "./core-components";
