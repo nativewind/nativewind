7fb6f954c7f3f281b779943c8af292efe4f83294
Mark Lawlor <mwlawlor@gmail.com>
2023-11-23 19:26:58 +1000

    update example

--- examples/expo-router/app/(tabs)/index.tsx
+++ examples/expo-router/app/(tabs)/index.tsx
@@ -8,9 +8,13 @@ const theme = vars({
 const App = () => {
   return (
     <View className="flex-1 items-center justify-center" style={theme}>
-      <Pressable className="ripple-purple-500 ripple-borderless bg-black">
-        <Text>Press Me</Text>
-      </Pressable>
+      <Text className="font-bold text-[--theme-fg]">Variables!</Text>
+      <Text className="font-bold active:scale-150 active:text-[--theme-fg] transition duration-[500ms]">
+        Transitions
+      </Text>
+      <Text className="font-bold animate-bounce placeholder:text-white">
+        Animations
+      </Text>
     </View>
   );
 };
