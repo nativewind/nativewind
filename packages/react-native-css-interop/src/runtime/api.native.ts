3610853e81ee2f379d81b8958dc128b4343764dd
Mark Lawlor <mwlawlor@gmail.com>
2024-01-15 21:42:43 +1000

    cleanup native api

--- packages/react-native-css-interop/src/runtime/api.native.ts
+++ packages/react-native-css-interop/src/runtime/api.native.ts
@@ -1,28 +1,17 @@
-import {
-  createElement,
-  forwardRef,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { useContext, useState } from "react";
 import type { CssInterop, JSXFunction, RuntimeValueDescriptor } from "../types";
 import { getNormalizeConfig } from "./config";
-import { usePropState } from "./native/use-prop-state";
-import { colorScheme, globalVariables } from "./native/globals";
+import { colorScheme } from "./native/globals";
+import { Effect, Observable } from "./observable";
+import { opaqueStyles } from "./native/stylesheet";
 import {
-  Effect,
-  Observable,
-  observable,
-  observableNotifyQueue,
-} from "./observable";
-import { globalStyles, opaqueStyles } from "./native/stylesheet";
-import { UpgradeState, renderComponent } from "./native/render-component";
+  createInteropComponent,
+  createRemapComponent,
+} from "./native/interop-component";
 import {
   InheritedParentContext,
   inheritanceContext,
 } from "./native/inherited-context";
-import { DEFAULT_CONTAINER_NAME } from "../shared";
 
 export const interopComponents = new Map<
   object | string,
@@ -43,199 +32,17 @@ export type ComponentState = InheritedParentContext & {
   };
 };
 
-export const cssInterop: CssInterop = (baseComponent, mapping): any => {
-  const configs = getNormalizeConfig(mapping);
-
-  const interopComponent = forwardRef(function CssInteropComponent(
-    originalProps: Record<string, any>,
-    ref: any,
-  ) {
-    const parent = useContext(inheritanceContext);
-
-    useEffect(() => {
-      const queue = [...observableNotifyQueue];
-      observableNotifyQueue.clear();
-      for (const sub of queue) {
-        sub();
-      }
-    });
-
-    const stateRef = useRef<ComponentState>();
-    if (!stateRef.current) {
-      const state: ComponentState = {
-        parent,
-        isAnimated: false,
-        inlineVariables: new Map(),
-        inlineContainerNames: new Set(),
-        container: observable(undefined),
-        getContainer(name, effect, isParent = false) {
-          if (isParent && state.inlineContainerNames.has(name)) {
-            return state.container;
-          }
-          return state.parent.getContainer(name, effect, true);
-        },
-        getVariable(name, effect) {
-          let value: any = undefined;
-          value ??= state.inlineVariables.get(name)?.get(effect);
-          value ??= globalVariables.universal.get(name)?.get(effect);
-          value ??= state.parent.getVariable(name, effect);
-          return value;
-        },
-        getActive(effect) {
-          state.active ??= observable(false, { name: "active" });
-          return state.active.get(effect);
-        },
-        getHover(effect) {
-          state.hover ??= observable(false);
-          return state.hover.get(effect);
-        },
-        getFocus(effect) {
-          state.focus ??= observable(false);
-          return Boolean(state.focus.get(effect));
-        },
-        getLayout(effect) {
-          state.layout ??= observable(undefined);
-          return state.layout.get(effect);
-        },
-        upgradeWarning: {
-          animated: UpgradeState.NONE,
-          context: UpgradeState.NONE,
-          pressable: UpgradeState.NONE,
-          canWarn: false,
-        },
-      };
-      state.container.set(state, false);
-      stateRef.current = state;
-    }
-    const state = stateRef.current!;
-    state.parent = parent;
-
-    const previousVariables = Array.from(state.inlineVariables.keys());
-    const previousContainers = Array.from(state.inlineContainerNames.keys());
-    state.inlineContainerNames.clear();
-    let props: Record<string, any> = { ...originalProps, ref };
-    let resetContext = false;
-
-    for (const config of configs) {
-      const result = usePropState(state, originalProps, config);
-
-      resetContext ||= result.resetContext;
-      state.isAnimated ||= result.isAnimated;
-
-      /**
-       * Merge the variables
-       */
-      for (const [name, value] of result.variables) {
-        let variable = state.inlineVariables.get(name);
-        if (!variable) {
-          state.inlineVariables.set(name, observable(value));
-        } else {
-          variable.set(value, false); // Do not immediately notify
-        }
-      }
-
-      /**
-       * Merge the container names
-       */
-      if (result.containerNames === null) {
-        state.inlineContainerNames.clear();
-      } else if (result.containerNames.length) {
-        for (const name of result.containerNames) {
-          state.inlineContainerNames.add(name);
-        }
-        state.inlineContainerNames.add(DEFAULT_CONTAINER_NAME);
-      }
-
-      Object.assign(props, result.props);
-      if (config.target !== config.source) {
-        delete props[config.source];
-      }
-    }
-
-    /**
-     * Cleanup old variables
-     */
-    for (const name of previousVariables) {
-      if (!state.inlineVariables.has(name)) {
-        state.inlineVariables.delete(name);
-      }
-    }
-
-    /**
-     * Cleanup old containers
-     */
-    if (previousContainers.length && state.inlineContainerNames.size === 0) {
-      // If we previously had containers, but now we don't
-      state.container.set(undefined, false);
-    } else if (state.inlineContainerNames.size) {
-      // If we previously had containers, but now we do
-      state.container.set(state, false);
-      state.layout ??= observable(undefined);
-      state.active ??= observable(false);
-      state.hover ??= observable(false);
-      state.focus ??= observable(false);
-    }
-
-    return renderComponent(
-      baseComponent,
-      state,
-      props,
-      originalProps,
-      resetContext,
-    );
-  });
-
-  interopComponent.displayName = `CssInterop.${
-    baseComponent.displayName ?? baseComponent.name ?? "unknown"
-  }`;
-  interopComponents.set(baseComponent, interopComponent);
+export const cssInterop: CssInterop = (component, mapping): any => {
+  const config = getNormalizeConfig(mapping);
+  const interopComponent = createInteropComponent(component, config);
+  interopComponents.set(component, interopComponent);
   return interopComponent;
 };
 
 export const remapProps: CssInterop = (component: any, mapping): any => {
-  const configs = getNormalizeConfig(mapping);
-
-  const interopComponent = forwardRef(function RemapPropsComponent(
-    { ...props }: Record<string, any>,
-    ref: any,
-  ) {
-    for (const config of configs) {
-      let rawStyles = [];
-
-      const source = props?.[config.source];
-
-      if (typeof source !== "string") continue;
-      delete props[config.source];
-
-      for (const className of source.split(/\s+/)) {
-        const signal = globalStyles.get(className);
-
-        if (signal !== undefined) {
-          const style = {};
-          opaqueStyles.set(style, signal.get());
-          rawStyles.push(style);
-        }
-      }
-
-      if (rawStyles.length !== 0) {
-        const existingStyle = props[config.target];
-
-        if (Array.isArray(existingStyle)) {
-          rawStyles.push(...existingStyle);
-        } else if (existingStyle) {
-          rawStyles.push(existingStyle);
-        }
-
-        props[config.target] =
-          rawStyles.length === 1 ? rawStyles[0] : rawStyles;
-      }
-    }
-
-    props.ref = ref;
-    return createElement(component as any, props, props.children);
-  });
-
-  interopComponents.set(component as any, interopComponent);
+  const config = getNormalizeConfig(mapping);
+  const interopComponent = createRemapComponent(component, config);
+  interopComponents.set(component, interopComponent);
   return interopComponent;
 };
 
