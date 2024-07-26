3dde5a63004ab81a7b160ebe3b2b16d95f65a8bd
Dan Stepanov <dan@danstepanov.com>
2024-07-26 01:11:05 -0700

    [WIP] specify path for jsx-runtime, install ts-jest, properly map to test-utils

--- packages/react-native-css-interop/jest.config.js
+++ packages/react-native-css-interop/jest.config.js
@@ -1,7 +1,16 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.json");
+
 module.exports = {
   preset: "jest-expo/ios",
   roots: ["src"],
   setupFiles: ["./src/utils/test-utils/setup.ts"],
   setupFilesAfterEnv: ["./src/utils/test-utils/setupAfterEnv.ts"],
   moduleDirectories: ["node_modules", "utils", __dirname],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  }
 };
