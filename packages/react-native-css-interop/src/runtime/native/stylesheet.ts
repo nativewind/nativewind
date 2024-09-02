6b0f2c3b27449eeea3f58948d99aad8df56d2b56
Mark Lawlor <mwlawlor@gmail.com>
2024-09-02 16:20:13 +1000

    add better darkmode tests

--- packages/react-native-css-interop/src/runtime/native/stylesheet.ts
+++ packages/react-native-css-interop/src/runtime/native/stylesheet.ts
@@ -1,8 +1,6 @@
 import { getStyle, injectData } from "./styles";
 import { CssInteropStyleSheet } from "../../types";
 import { flags } from "./globals";
-// import { ExtractedAnimation, StyleRuleSet } from "../../types";
-// import { Observable } from "../observable";
 
 export const StyleSheet: CssInteropStyleSheet = {
   getGlobalStyle(name: string) {
