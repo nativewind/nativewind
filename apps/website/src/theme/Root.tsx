0288bc8850cd8d377bda0343ccc9a05cd39ffbd8
Dan Stepanov <dan@danstepanov.com>
2025-04-17 14:50:04 -0400

    add vercel analytics

---
+++ apps/website/src/theme/Root.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
+
+export default function Root({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      {children}
+      <Analytics />
+    </>
+  );
+}
