5df04a911cc3773f1312809221b4b9c90736a9ec
Dan Stepanov <dan@danstepanov.com>
2025-06-10 12:34:38 -0400

    [WIP] remove example expo router app

--- examples/expo-router/app/(tabs)/two.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function TabTwoScreen() {
-  return (
-    <View>
-      <Text className="text-orange-400">Tab two!!</Text>
-    </View>
-  );
-}
