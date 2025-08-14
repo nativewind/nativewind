70d31430dd04738e5a2ab6b350a5cd086b479e93
Dean Simcox <deansimcox@gmail.com>
2025-08-14 23:09:54 +0800

    This declaration still seems important, extending it to address  type errors

---
+++ src/react-native-css.d.ts
@@ -0,0 +1,14 @@
+// TODO: Delete this when react=native-css is published
+declare module "react-native-css" {
+  export declare const styled;
+  export declare const setRem;
+  export declare const useCssElement;
+  export declare const useUnstableNativeVariable;
+  export type StyledConfiguration<T extends any> = any;
+  export type StyledProps<Props extends any, Mapping extends any> = any;
+};
+declare module "react-native-css/babel";
+declare module "react-native-css/metro" {
+  export declare const withReactNativeCSS: any;
+  export type WithReactNativeCSSOptions = any;
+}
