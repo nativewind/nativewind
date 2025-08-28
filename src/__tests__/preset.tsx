1df7f2ed98334d23c6c10645ec1c5c8ca76c1fef
Mark Lawlor <mwlawlor@gmail.com>
2025-08-28 17:14:05 +1000

    remove upstreamed tests

--- src/__tests__/preset.tsx
+++
@@ -1,9 +0,0 @@
-import { renderCurrentTest } from "../test-utils";
-
-describe("Preset - color-*", () => {
-  test("color-black", async () => {
-    expect(await renderCurrentTest()).toStrictEqual({
-      props: { style: { color: "#000" } },
-    });
-  });
-});
