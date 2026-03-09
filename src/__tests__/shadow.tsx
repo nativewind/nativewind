7c015dc8a34714de07b6c76f915ab16fd4d737ab
Yevhenii <evgeny.kotyrlo@gmail.com>
2026-03-09 12:02:30 +0200

    test: add shadow utility validation tests

---
+++ src/__tests__/shadow.tsx
@@ -0,0 +1,68 @@
+import { renderCurrentTest, renderSimple } from "../test-utils";
+
+describe("Shadow utilities", () => {
+  test("shadow-sm", async () => {
+    const result = await renderCurrentTest();
+    expect(result.props.style.boxShadow).toStrictEqual([
+      {
+        offsetX: 0,
+        offsetY: 1,
+        blurRadius: 3,
+        spreadDistance: 0,
+        color: "#0000001a",
+      },
+      {
+        offsetX: 0,
+        offsetY: 1,
+        blurRadius: 2,
+        spreadDistance: -1,
+        color: "#0000001a",
+      },
+    ]);
+  });
+
+  test("shadow-md", async () => {
+    const result = await renderCurrentTest();
+    expect(result.props.style.boxShadow).toStrictEqual([
+      {
+        offsetX: 0,
+        offsetY: 4,
+        blurRadius: 6,
+        spreadDistance: -1,
+        color: "#0000001a",
+      },
+      {
+        offsetX: 0,
+        offsetY: 2,
+        blurRadius: 4,
+        spreadDistance: -2,
+        color: "#0000001a",
+      },
+    ]);
+  });
+
+  test("shadow-lg", async () => {
+    const result = await renderSimple({ className: "shadow-lg" });
+    expect(result.props.style.boxShadow).toStrictEqual([
+      {
+        offsetX: 0,
+        offsetY: 10,
+        blurRadius: 15,
+        spreadDistance: -3,
+        color: "#0000001a",
+      },
+      {
+        offsetX: 0,
+        offsetY: 4,
+        blurRadius: 6,
+        spreadDistance: -4,
+        color: "#0000001a",
+      },
+    ]);
+  });
+
+  test("shadow-none", async () => {
+    const result = await renderCurrentTest();
+    expect(result.props.style.boxShadow).toStrictEqual([]);
+  });
+});
